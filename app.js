b9d769bcbbfa454686fbbd18d626cd6d0378fef7
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-05-28 18:41:07 +0330

    add new routes and ejs

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const routes = require("./routes/routes");
 
 const app = express();
 
-// app.set("view engine", "ejs");
-// app.set("views", "views");
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: req.session.user });
+  res.render("index");
 });
 
-router.post("/api", (req, res) => {
-  req.session.user = req.query;
+router.post("/login", (req, res) => {
+  req.session.user = req.body;
+  res.render("index");
+});
+
+router.get("/check", (req, res) => {
   res.json({ message: req.session.user });
 });
 
